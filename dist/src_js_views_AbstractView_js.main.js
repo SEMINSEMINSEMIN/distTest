68d42872459773b02097d4f75c39f3455e92f97d
강세민 <ksm9802@gmail.com>
2023-02-18 18:43:14 +0900

    html script src js/main.js에서 main.js로

---
+++ dist/src_js_views_AbstractView_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkdistributetest"] = self["webpackChunkdistributetest"] || []).push([["src_js_views_AbstractView_js"],{
+
+/***/ "./src/js/views/AbstractView.js":
+/*!**************************************!*\
+  !*** ./src/js/views/AbstractView.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class {\n    constructor(params) {\n        this.params = params;\n    }\n\n    setTitle(title) {\n        document.title = title;\n    }\n\n    async getHTML() {\n        return \"\";\n    }\n});\n\n//# sourceURL=webpack://distributetest/./src/js/views/AbstractView.js?");
+
+/***/ })
+
+}]);
